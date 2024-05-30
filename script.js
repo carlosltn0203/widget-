a64ef697c2b9da60a8fe52b1a00c399806bb0fe5
carlosltn0203 <145719493+carlosltn0203@users.noreply.github.com>
2024-05-30 16:16:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#fecha").datepicker();
+
+    $("#buscarDisponibilidad").click(function() {
+        var fechaSeleccionada = $("#fecha").val();
+
+        $.ajax({
+            url: "https://github.com/carlosltn0203/json-/blob/main/api/dummy.js",
+            method: "GET",
+            data: { fecha: fechaSeleccionada },
+            success: function(data) {
+                mostrarDisponibilidad(data);
+            },
+            error: function() {
+                alert("Error al obtener la disponibilidad");
+            }
+        });
+    });
+
+    function mostrarDisponibilidad(data) {
+        var resultadoHTML = "<h2>Disponibilidad para " + data.fecha + "</h2>";
+        resultadoHTML += "<ul>";
+
+        $.each(data.personal_disponible, function(index, empleado) {
+            resultadoHTML += "<li>";
+            resultadoHTML += "<strong>Nombre:</strong> " + empleado.nombre + "<br>";
+            resultadoHTML += "<strong>Horario:</strong> " + empleado.horarios.join(", ") + "<br>";
+            resultadoHTML += "<strong>ID:</strong> " + empleado.id;
+            resultadoHTML += "</li>";
+        });
+
+        resultadoHTML += "</ul>";
+
+        $("#resultado").html(resultadoHTML);
+    }
+});
